8850f65ef5177bad48f6f9a800348af71576c543
linchunhan <linhog5555@gmail.com>
2024-11-23 23:07:59 +0800

    Initial Commit

---
+++ game/gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  gameApp.swift
+//  game
+//
+//  Created by 林君翰 on 2024/11/23.
+//
+
+import SwiftUI
+
+@main
+struct gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ gameTests/gameTests.swift
@@ -0,0 +1,17 @@
+//
+//  gameTests.swift
+//  gameTests
+//
+//  Created by 林君翰 on 2024/11/23.
+//
+
+import Testing
+@testable import game
+
+struct gameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
